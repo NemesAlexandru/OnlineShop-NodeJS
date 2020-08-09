bfa2f76e1dd80f280c0d830b5edbe7a5a7a8a7ad
NemesAlexandru <alexandrunemes1@gmail.com>
2020-08-09 15:41:21 +0300

    Worked on ejs templates for user authentication, created routes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
+
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server started on ${PORT}`))
+app.listen(PORT, console.log(`Server started on ${PORT}`));
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+//for registration
+
+const express = require('express');
+const router = express.Router();
+
+//Login Page
+router.get('/login', (req, res) => res.render('login'));
+
+//Register Page
+router.get('/register', (req, res) => res.render('register'));
+
+module.exports = router;
