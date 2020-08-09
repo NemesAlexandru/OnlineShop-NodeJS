8ba8bd5518d3c7435bb4ab4f9956d7b148bfa63b
NemesAlexandru <alexandrunemes1@gmail.com>
2020-08-09 16:26:51 +0300

    created mongoose user model, connection to Atlas, user registration validation

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//DB config
+const db = require('./config/keys').MongoURI;
 
+//Connect to Mongo
+mongoose.connect(db, { useUnifiedTopology: true, 
+    useNewUrlParser: true }).then(() => 
+    console.log('MongoDB connected...')).catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 //for registration
 
 const express = require('express');
+const { route } = require('.');
 const router = express.Router();
 
 //Login Page
@@ -8,5 +9,41 @@ router.get('/login', (req, res) => res.render('login'));
 
 //Register Page
 router.get('/register', (req, res) => res.render('register'));
+
+//Register Handle
+router.post('/register', (req, res) =>{
+   const { name, email, password, password2 } = req.body;
+let errors = [];
+
+//check required fields
+if(!name || !email || !password || !password2){
+    errors.push({ msg: 'Please fill in all fields' });
+}
+
+//check passwords match
+
+if(password !== password2){
+    errors.push({ msg: 'Passwords do not match' });
+}
+
+//check pass length
+if(password.length < 6){
+    errors.push({ msg: 'Password should be at least 6 characters' });
+}
+
+if(errors.length > 0){
+res.render('register', {
+errors,
+name,
+email,
+password,
+password2
+});
+}else {
+    res.send('pass');
+}
+
+});
+
 
 module.exports = router;
