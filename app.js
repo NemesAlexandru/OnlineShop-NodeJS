58fd40bcf76dc79fd862166513d98df933bc5f63
NemesAlexandru <alexandrunemes1@gmail.com>
2020-08-10 12:34:28 +0300

    added flash messages, session

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -18,6 +20,23 @@ app.set('view engine', 'ejs');
 
 //Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+//Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+  //Connect flash
+  app.use(flash());
+
+  //Global vars
+  app.use((req, res, next) => {
+      res.locals.success_msg = req.flash('success_msg');
+      res.locals.error_msg = req.flash('error_msg');
+      next();
+  });
 
 //Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -72,6 +72,7 @@ bcrypt.genSalt(10, (err, salt) => bcrypt.hash(newUser.password, salt,
         //save user
         newUser.save()
         .then(user => {
+            req.flash('success_msg', 'You are now registered and can log in');
             res.redirect('/users/login');
         })
         .catch(err => console.log(err));
