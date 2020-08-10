9885533df090c11cd43a729ae7dad78501e8ee81
NemesAlexandru <alexandrunemes1@gmail.com>
2020-08-10 13:29:57 +0300

    implemented authorization

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
+
 
 //Welcome page
 router.get('/', (req, res) => res.render('welcome'));
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+//Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+res.render('dashboard', {
+    name: req.user.name
+}));
 
 module.exports = router;
